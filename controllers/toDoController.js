4443c053356c2b812161667f84165525379c6875
Praful Kumar Jha <prafuljh@gmail.com>
2017-02-02 16:50:52 +0530

    Add files via upload

--- controllers/toDoController.js
+++ controllers/toDoController.js
@@ -3,35 +3,57 @@
  */
 
  var bodyParser = require("body-parser");
+ var replaceall = require("replaceall");
+ var mongoose = require("mongoose");
+ mongoose.connect("mongodb://test:test@ds139969.mlab.com:39969/todoap");
+ var todoSchema = new mongoose.Schema({
+	 item : String
+ });
+
+var Todo =  mongoose.model("Todo",todoSchema);
+/*var itemOne = TodoAp({
+	item : "Buy Flowers"
+}).save(function(err) {
+	if(err)
+	throw err;
+	console.log("item saved");
+});
+*/
+
  // create application/x-www-form-urlencoded parser
 var urlencodedParser = bodyParser.urlencoded({ extended: false });
 
-var data = [{item:"get milk"},{item:"Work Out"}, {item:"Wake up early"}];
+//var data = [{item:"get milk"},{item:"Work Out"}, {item:"Wake up early"}];
 
 module.exports = function (app) {
-
-
-	//GET
-
+//GET
 	app.get("/todo",function(req,res) {
-		res.render("todo",{todos:data});
+	  Todo.find({}, function(err,data) {
+			if(err)	throw err;
+			console.log("Got all the Items");
+			res.render("todo",{todos:data});
+		});
 	});
 
 	//Delete
 	app.delete("/todo/:item",function(req,res) {
-		data = data.filter(function(todo) {
+		var reqItem = replaceall("-"," ",req.params.item);
+		reqItem = reqItem.trim();
+		Todo.find({item: reqItem}).remove(function(err,data) {
+			if(err) throw err;
+			console.log("Item Removed");
+			res.json(data);
+		})
 
-			return todo.item.replace(/ /g,'-') !== req.params.item;
-		});
-		console.log(data);
-res.render("todo",{todos:data});
 	});
 
 //POST
 	app.post("/todo",urlencodedParser,function(req,res) {
-		data.push(req.body);
-		res.render("todo",{todos:data});
-
+    var newTodo = Todo(req.body).save(function(err,data) {
+			if(err)	throw err;
+			console.log("New Item Added");
+			res.json(data);
+		})
 	});
 
 };
